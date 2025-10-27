f5f7810164d4a6bef8bbddbcb4c2271d855e68e4
Leonardo Valencia <leov@gammawizard.com>
2025-10-27 11:53:02 -0400

    Agregando manejo de pantalla

---
+++ memoria/Program.fs
@@ -0,0 +1,33 @@
+﻿
+open System
+
+
+let width = Console.BufferWidth // Ancho del buffer de la consola
+let height = Console.BufferHeight // Alto del buffer de la consola
+
+
+let oldForeGround = Console.ForegroundColor
+let oldBackground = Console.BackgroundColor
+Console.ForegroundColor <- ConsoleColor.Yellow
+Console.BackgroundColor <- ConsoleColor.DarkBlue
+Console.Clear() // Limpia la consola
+
+Console.SetCursorPosition(width-10,height/2)
+
+Console.WriteLine "Hola Mundo"
+Console.ReadLine() |> ignore
+Console.ForegroundColor <- oldForeGround
+Console.BackgroundColor <- oldBackground
+Console.Clear()
+
+let mostrarMensaje x y (mensaje:string)=
+    Console.SetCursorPosition(x,y)
+    Console.WriteLine mensaje
+
+let borrarMensaje x y (mensaje:string)=
+    Console.SetCursorPosition(x,y)
+    [1..mensaje.Length]
+    |> Seq.iter (fun _ -> Console.Write " ")
+
+
+
