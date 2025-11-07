89fcd857003a075d7bc18bcf560d85bb227f46a1
Leonardo Valencia <leov@gammawizard.com>
2025-11-07 10:00:23 -0500

    Juego de Alien casi terminado

--- animation1/Program.fs
+++ animation1/Program.fs
@@ -12,7 +12,7 @@ type ProgramState =
 | Running
 | Terminated
 
-type EnemyState =
+type SpriteState =
 | Vivo
 | Colisionado
 | Dormido
@@ -27,6 +27,9 @@ type State = {
     Height: int
     AlienX: int
     AlienY: int
+    AlienState: SpriteState
+    AlienColisionTime: int
+    AlienLives: int
     Counter: int
     Tick: int
     ProgramState: ProgramState
@@ -34,8 +37,12 @@ type State = {
     EnemyX: int
     EnemyY: int
     EnemySpeed: float
-    EnemyState: EnemyState
+    EnemyState: SpriteState
+    EnemyMisiles: Misil list
+    LastMisileTime: int
     ColisionTime: int
+    EnemyT0: int
+    Puntaje: int
 }
 
 let initSate() =
@@ -44,6 +51,9 @@ let initSate() =
         Height = Console.BufferHeight
         AlienY = Console.BufferHeight/2
         AlienX = Console.BufferWidth/2
+        AlienState = Vivo
+        AlienColisionTime = 0
+        AlienLives = 3
         ProgramState = Running
         Counter = 0
         Tick =0
@@ -52,7 +62,11 @@ let initSate() =
         EnemyY = 0
         EnemySpeed = Math.PI/50.0
         EnemyState = Vivo
+        EnemyMisiles = []
         ColisionTime = 0
+        EnemyT0 = 0
+        Puntaje = 0
+        LastMisileTime = 0
     }
 
 
@@ -75,7 +89,10 @@ let dormirUnMomento() =
 
 
 let displayAlien state =
-    displayMessage state.AlienX state.AlienY ConsoleColor.Yellow "👽"
+    match state.AlienState with
+    | Vivo -> displayMessage state.AlienX state.AlienY ConsoleColor.Yellow "👽"
+    | Colisionado -> displayMessage state.AlienX state.AlienY ConsoleColor.Yellow "⚰️"
+    | _ -> ()
     state
 
 let displayCounter state =
@@ -87,22 +104,38 @@ let displayMisil state =
     |> List.iter ( fun m -> displayMessage m.X m.Y ConsoleColor.Cyan "=>")
     state
 
-let displayEnemy state =
-    if state.EnemyState = Vivo then 
-        displayMessage state.EnemyX state.EnemyY ConsoleColor.Yellow "👾"
+let displayEnemyMisil state =
+    state.EnemyMisiles
+    |> List.iter ( fun m -> displayMessage m.X m.Y ConsoleColor.Yellow "<=")
     state
 
+let displayEnemy state =
+    match state.EnemyState with  
+    | Vivo -> displayMessage state.EnemyX state.EnemyY ConsoleColor.Yellow "👾"
+    | Colisionado -> displayMessage state.EnemyX state.EnemyY ConsoleColor.Yellow "💥" 
+    | _ ->()
+    state
+
+let displayScore state =
+    displayMessage 0 0 ConsoleColor.Yellow $"Score: {state.Puntaje}"
+    state
+
+let displayLives state =
+    displayMessage (state.Width/2-10) 0 ConsoleColor.Yellow $"Lives: {state.AlienLives}"
 let updateAlienKeyboard key state =
-    match key with
-    | ConsoleKey.LeftArrow ->
-        {state with AlienX= max 0 (state.AlienX-1)} // Guards
-    | ConsoleKey.RightArrow ->
-        { state with AlienX = min (state.Width-2) (state.AlienX+1)}
-    | ConsoleKey.UpArrow ->
-        { state with AlienY = max 0 (state.AlienY-1)}
-    | ConsoleKey.DownArrow ->
-        {state with AlienY = min (state.Height-1) (state.AlienY+1) }
-    | _ -> state
+    if state.AlienState = Vivo then
+        match key with
+        | ConsoleKey.LeftArrow ->
+            {state with AlienX= max 0 (state.AlienX-1)} // Guards
+        | ConsoleKey.RightArrow ->
+            { state with AlienX = min (state.Width-2) (state.AlienX+1)}
+        | ConsoleKey.UpArrow ->
+            { state with AlienY = max 0 (state.AlienY-1)}
+        | ConsoleKey.DownArrow ->
+            {state with AlienY = min (state.Height-1) (state.AlienY+1) }
+        | _ -> state
+    else
+        state
 
 let updateScape key state =
     match key with 
@@ -111,11 +144,21 @@ let updateScape key state =
     | _ -> state
 
 let udpateMisilKeyboard key state =
-    match key with
-    | ConsoleKey.Spacebar ->
-        let nuevoMisil = {X=state.AlienX+2;Y=state.AlienY}
-        {state with Misiles = nuevoMisil :: state.Misiles}
-    | _ -> state
+    if state.AlienState = Vivo then
+        match key with
+        | ConsoleKey.Spacebar ->
+            let nuevoMisil = {X=state.AlienX+2;Y=state.AlienY}
+            {state with Misiles = nuevoMisil :: state.Misiles}
+        | _ -> state
+    else
+        state
+
+let dispararMisilEnemigo state =
+    if state.Tick - state.LastMisileTime >= 6 then 
+        let nuevoMisil = {X=state.EnemyX-2;Y=state.EnemyY}
+        {state with EnemyMisiles = nuevoMisil :: state.EnemyMisiles; LastMisileTime = state.Tick}
+    else
+        state
 
 let updateKeyboard state =
     if Console.KeyAvailable then
@@ -129,12 +172,15 @@ let updateKeyboard state =
 
 
 let clearAlien state =
-    displayMessage state.AlienX state.AlienY ConsoleColor.Yellow "  "
+    match state.AlienState with 
+    | Vivo | Colisionado -> displayMessage state.AlienX state.AlienY ConsoleColor.Yellow "  "
+    | _ -> ()
     state
 
 let clearEnemy state =
-    if state.EnemyState = Vivo then 
-        displayMessage state.EnemyX state.EnemyY ConsoleColor.Yellow "  "
+    match state.EnemyState with 
+    | Vivo | Colisionado -> displayMessage state.EnemyX state.EnemyY ConsoleColor.Yellow "  "
+    | _ -> ()
     state
 
 let clearMisil state =
@@ -144,10 +190,18 @@ let clearMisil state =
     )
     state
 
+let clearEnemyMisil state =
+    state.EnemyMisiles
+    |> List.iter (fun m -> 
+        displayMessage m.X m.Y ConsoleColor.Cyan "  "
+    )
+    state
+
 let clearOldObjects state =
     state
     |> clearAlien
     |> clearMisil
+    |> clearEnemyMisil
     |> clearEnemy
     |> ignore
 
@@ -168,10 +222,19 @@ let updateMisil state =
         |> Seq.toList
     { state with Misiles = nuevosMisiles}
 
+let updateEnemyMisil state =
+    let nuevosMisiles =
+        state.EnemyMisiles
+        |> Seq.map ( fun m -> {m with X = m.X-1})
+        |> Seq.filter ( fun m -> m.X >= 0)
+        |> Seq.toList
+    { state with EnemyMisiles = nuevosMisiles}
+
 let updateEnemy state =
     if state.EnemyState = Vivo then
-        let nuevoY = - float state.Height/2.0*(Math.Cos (float state.Tick*state.EnemySpeed) - 1.0)
+        let nuevoY = - float state.Height/2.0*(Math.Cos (float (state.Tick - state.EnemyT0)*state.EnemySpeed) - 1.0)
         {state with EnemyY = min (state.Height-1) (int nuevoY)}
+        |> dispararMisilEnemigo
     else
         state
 
@@ -182,24 +245,66 @@ let updateColision state =
         |> List.filter (fun m -> not (m.X+1=state.EnemyX && m.Y=state.EnemyY))
         |> fun nuevaLista ->
             if nuevaLista.Length <> state.Misiles.Length then
-                {state with Misiles=nuevaLista;EnemyState=Colisionado}
+                {state with 
+                    Misiles=nuevaLista
+                    EnemyState=Colisionado
+                    ColisionTime = state.Tick
+                    Puntaje = state.Puntaje+1
+                }
             else
                 state
-    | Colisionado -> 
-        {state with ColisionTime = state.Counter; EnemyState=Dormido} // la magia ocurre aqui
+    | Colisionado ->
+        if state.Tick - state.ColisionTime >= 25 then 
+            {state with EnemyState=Dormido}
+        else
+            state // la magia ocurre aqui
     | Dormido ->
-        if state.Counter-state.ColisionTime >= 4 then
-            {state with EnemyState = Vivo}
+        if state.Tick-state.ColisionTime >= 100 then
+            {state with EnemyState = Vivo; EnemyT0 = state.Tick}
         else
             state
 
+let updateAlienColision state =
+    match state.AlienState with 
+    | Vivo ->
+        state.EnemyMisiles
+        |> List.filter (fun m -> not (m.X=state.AlienX && m.Y=state.AlienY))
+        |> fun nuevaLista ->
+            if nuevaLista.Length <> state.EnemyMisiles.Length then
+                {state with 
+                    EnemyMisiles=nuevaLista
+                    AlienState=Colisionado
+                    AlienColisionTime = state.Tick
+                    AlienLives = state.AlienLives-1
+                }
+            else
+                state
+    | Colisionado ->
+        if state.Tick - state.AlienColisionTime >= 25 then 
+            {state with AlienState=Dormido}
+        else
+            state // la magia ocurre aqui
+    | Dormido ->
+        if state.Tick-state.AlienColisionTime >= 100 then
+            {state with AlienState = Vivo;AlienX = state.Width/2; AlienY = state.Height/2}
+        else
+            state
+
+let updateVidas state =
+    if state.AlienLives <= 0 then
+        {state with ProgramState = Terminated}
+    else
+        state
 let updateState state =
     state
     |> updateTick
     |> updateCounter
     |> updateMisil
+    |> updateEnemyMisil
     |> updateEnemy
     |> updateColision
+    |> updateAlienColision
+    |> updateVidas
     |> updateKeyboard
 
 let updateScreen state =
@@ -207,7 +312,10 @@ let updateScreen state =
     |> displayCounter 
     |> displayAlien
     |> displayMisil
+    |> displayEnemyMisil
     |> displayEnemy
+    |> displayScore
+    |> displayLives
     |> ignore
 
 let rec mainLoop state =
